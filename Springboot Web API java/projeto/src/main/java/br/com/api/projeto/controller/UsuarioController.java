808c4f8a372d084c7a543ace8ea3dedf0e27a696
victor-delfino <hugo.delfino11@gmail.com>
2024-01-22 15:27:54 -0300

    Adição da criptografia para as senhas

--- Springboot Web API java/projeto/src/main/java/br/com/api/projeto/controller/UsuarioController.java
+++ Springboot Web API java/projeto/src/main/java/br/com/api/projeto/controller/UsuarioController.java
@@ -2,7 +2,7 @@ package br.com.api.projeto.controller;
 
 import java.util.List;
 
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -46,5 +46,14 @@ public class UsuarioController {
     public ResponseEntity <?> deletarUsuario(@PathVariable Integer id) {
         usuarioService.deletarUsuario(id);
         return ResponseEntity.status(204).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Usuario> validarSenha(@RequestBody Usuario usuario) {
+        Boolean valid = usuarioService.validarSenha(usuario);
+        if(!valid) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.status(200).build();
     }
 }
--- Springboot Web API java/projeto/src/main/java/br/com/api/projeto/service/UsuarioService.java
+++ Springboot Web API java/projeto/src/main/java/br/com/api/projeto/service/UsuarioService.java
@@ -2,6 +2,8 @@ package br.com.api.projeto.service;
 
 import java.util.List;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.api.projeto.DAO.InterfaceUsuario;
@@ -11,9 +13,11 @@ import br.com.api.projeto.entidade.Usuario;
 public class UsuarioService {
 
     private InterfaceUsuario dao;
+    private PasswordEncoder passwordEncoder;
 
     public UsuarioService(InterfaceUsuario dao) {
         this.dao = dao;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public List<Usuario> listarUsuario() {
@@ -22,16 +26,26 @@ public class UsuarioService {
     }
 
     public Usuario criarUsuario(Usuario usuario) {
+        String encoder = this.passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(encoder);
         Usuario usuarioNovo = dao.save(usuario);
         return usuarioNovo;
     }
 
     public Usuario alterarUsuario(Usuario usuario) {
+        String encoder = this.passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(encoder);
         Usuario usuarioNovo = dao.save(usuario);
         return usuarioNovo;
     }
     public Boolean deletarUsuario(Integer id) {
         dao.deleteById(id);
         return true;
+    }
+
+    public Boolean validarSenha(Usuario usuario) {
+        String senha = dao.getById(usuario.getId()).getSenha();
+        Boolean valid = passwordEncoder.matches(usuario.getSenha(), senha);
+        return valid;
     }
 }
